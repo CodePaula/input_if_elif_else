6340bf7566bf65f7cca97dad28fe4706bba66f61
Paula Yagnycz <123567908+CodePaula@users.noreply.github.com>
2023-02-10 11:47:01 -0300

    Update Exercício com input e if _ Exercise with input and if.py

--- Exercício com input e if _ Exercise with input and if.py
+++ Exercício com input e if _ Exercise with input and if.py
@@ -4,14 +4,21 @@
 # In[ ]:
 
 
+# adicionando a informação solicitada
 turno = input("Em qual turno você estuda? Digite M para matutino, V para vespertino ou N para noturno. ")
 
+# as condicionais vão testar se a resposta digitada corresponde às opções aceitas pela variável "turno" (M, V ou N)
+# se for M, o resultado será "Bom dia!"
 if turno == "M":
     print("Bom dia!")
+# como mais de uma condição precisa ser testada, usamos elif
+# se for V, o resultado será "Boa tarde!"
 elif turno == "V":
     print("Boa tarde!")
+# se for N, o resultado será "Boa noite!"
 elif turno == "N":
     print("Boa noite!")
+# se nenhuma das condições forem atendidas (M, V ou N), o resultado será inválido
 else:
     print("Valor inválido.")
 
@@ -19,14 +26,21 @@ else:
 # In[ ]:
 
 
+# adding the requested information
 shift = input("In which shift do you study? Type M for morning, V for vespertine or N for night. ")
 
+# the conditionals will test if the typed answer corresponds with the accepted options of the variable "shift" (M, V ou N)
+# if it is M, the outcome will be "Good morning!"
 if shift == "M":
     print("Good morning!")
+# as more than one conditional needs to be tested, we use elif
+# if it is V, the outcome will be "Good afternoon!"
 elif shift == "V":
     print("Good afternoon!")
+# if it is N, the outcome will be "Good evening!"
 elif shift == "N":
     print("Good evening!")
+# if none of the conditionals were satisfied (M, V or N), the outcome will be invalid
 else:
     print("Invalid value.")
 
